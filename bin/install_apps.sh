418ebdb76f6ff63a2d55d724829b2b2ffcfc9642
Fredrik Kjellberg <fredrik.kjellberg@saabgroup.com>
2024-01-16 09:29:13 +0000

    Add start of a script to install all needed applications (it basically just lists them now)

---
+++ bin/install_apps.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# neovim
+
+# vscode
+
+# vscode extensions
+#johnstoncode.svn-scm
+#medo64.render-crlf
+#moshfeu.compare-folders
+#ms-python.python
+#ms-python.vscode-pylance
+#ms-vscode-remote.remote-ssh
+#ms-vscode-remote.remote-ssh-edit
+##ms-vscode.cpptools
+#ms-vscode.remote-explorer
+#redhat.vscode-xml
+##rust-lang.rust-analyzer
+##TabbyML.vscode-tabby
+##vscodevim.vim
+
+# fish
+
+# eza
+
+# bat
+
+# fd-find
+
+# ripgrep
+
+# zoxide
+
+# fzf
+
+# qtcreator
+
+# i3
+
+# clang-format
+
+# git-svn
+
+# cargo
+
+# gitui
+
+# docker
+
+# stow
